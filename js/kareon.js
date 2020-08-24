e23ce5b89e4466000118169a80416322eef14d39
Amorue Daniel <amoruedaniel@gmail.com>
2020-08-24 11:59:10 +0800

    commited by daniel

--- js/kareon.js
+++ js/kareon.js
@@ -26,3 +26,61 @@ document.getElementById("menu-toggle").onclick = function(){
 }
 
 
+
+/*==========Work on contact form =========*/
+
+const form = document.getElementById("form");
+const fullName = document.getElementById("yourFullName");
+const email = document.getElementById("yourEmail");
+const phone = document.getElementById("yourPhone");
+const message = document.getElementById("yourMessage");
+
+form.addEventListener('submit',(e)=>{
+	e.preventDefault();
+	checkInputs();
+});
+
+let checkInputs = () =>{
+	const fullNameValue = fullName.value.trim();
+	const emailValue = email.value.trim();
+	const phoneValue = phone.value.trim();
+	const messageValue = message.value.trim();
+
+	if(fullNameValue === ""){
+		//show Error
+		//add error class
+		setErrorFor(fullName, "Please input your full name");
+	}
+	else{
+		//add success class
+		setSuccessFor(fullName);
+	}
+
+	if(emailValue === ""){
+		setErrorFor(email, 'Email cannot be blank');
+	}
+	else if(!isEmail(emailValue)){
+		setErrorFor(email, 'Email is not valid');
+	}
+	else{
+		setSuccessFor(email);
+	}
+}
+function setErrorFor(input, message){
+	const formGroup = input.parentElement; //.form-group
+	const small = formGroup.querySelector("small");
+
+	//add error message inside small
+	small.innerText = message;
+
+	//add error class
+	formGroup.classList = "form-group error";
+}
+function setSuccessFor(input){
+	const formGroup = input.parentElement;
+	formGroup.className = "form-group sucess"
+}
+
+function isEmail(email){
+	return  /^(([^<>()[]\.,;:s@"]+(.[^<>()[]\.,;:s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/igm;if ($email.val() == '' || !re.test($email.val()));
+}
