1bb353bf7a55b4996d340aba7fba8947d121bcfa
Amorue Daniel <amoruedaniel@gmail.com>
2020-08-28 12:09:39 +0800

    comitted by daniel

--- js/kareon.js
+++ js/kareon.js
@@ -1,4 +1,39 @@
- /*//Scroll to hide shrink
+ /*show mobile navigation bar*/
+ document.getElementById("menu-toggle").onclick = function(){
+	document.querySelector("nav").classList.toggle("active");
+}
+
+/*scroll to top*/
+const toTop = document.querySelector(".to-top");
+window.addEventListener("scroll",()=>{
+	if(window.pageYOffset > 100){
+		toTop.classList.add("active");
+	}else{
+		toTop.classList.remove("active");
+	}
+})
+
+/*this adds margin at the top of every top section to avoid them hiding under the vav bar
+let marginAdder = () =>{
+	var navHeight = document.getElementById("header").offsetHeight;
+	if(screen.width <=768){
+		document.querySelector(".top-section").style.marginTop = navHeight + 15 + "px";
+	}
+	else if(screen.width > 768 && screen.width <= 992){
+		document.querySelector(".top-section").style.marginTop = navHeight + 15 + "px";
+	}
+	else{
+		document.querySelector(".top-section").style.marginTop = navHeight + 20 + "px";
+	}
+	console.log(navHeight)
+}
+marginAdder();*/
+
+/*======Photoswipe js code=====*/
+
+
+
+ /*Scroll to hide shrink
  window.addEventListener("scroll", function(){
         var navbar = document.querySelector("nav");
         navbar.classList.toggle("sticky",window.scrollY>0);
@@ -20,14 +55,13 @@
  }
  */
 
- /*show mobile navigation bar*/
-document.getElementById("menu-toggle").onclick = function(){
-	document.querySelector("nav").classList.toggle("active");
-}
 
 
 
-/*==========Work on contact form =========*/
+
+
+
+/*==========Work on contact form =========
 
 const form = document.getElementById("form");
 const fullName = document.getElementById("yourFullName");
@@ -84,3 +118,6 @@ function setSuccessFor(input){
 function isEmail(email){
 	return  /^(([^<>()[]\.,;:s@"]+(.[^<>()[]\.,;:s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/igm;if ($email.val() == '' || !re.test($email.val()));
 }
+
+
+*/
